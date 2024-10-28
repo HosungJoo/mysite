3c6581ceaf9e82863f3698d4deb381f36e9e0dda
wnghtjd303 <wnghtjd303@naver.com>
2024-10-28 15:03:58 +0900

    django admin update

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+#Client로부터 request를 받아서 HttoResponse로 return하는게 기본
+#그 사이에 부가적인 기능이 들어갈 수도
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world.")
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
